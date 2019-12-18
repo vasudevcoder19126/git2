ebeb5c45bd22b3467b5993c2c8baa3dc90e2c64a
vasudevcoder19126 <vasudev19126@iiitd.ac.in>
2019-12-19 01:29:21 +0530

    add a line

--- scratch_1.java
+++ scratch_1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class Scratch {
     public static void main(String[] args) {
+    	System.out.print("i am best");
         Scanner scr= new Scanner(System.in);
         int n=scr.nextInt();
         int sp=1;
