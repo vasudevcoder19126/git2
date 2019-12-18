abfed7e2d981e04ab3969ed4771c15a732754638
vasudevcoder19126 <55682741+vasudevcoder19126@users.noreply.github.com>
2019-12-19 00:44:55 +0530

    my java file is been added

---
+++ scratch_1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scanner scr= new Scanner(System.in);
+        int n=scr.nextInt();
+        int sp=1;
+        int st=n/2;
+        for(int i=1;i<=n;i++){
+            int j=1;
+            while(j<=st){
+                System.out.print("* ");
+                j++;
+            }
+            j=1;
+            while(j<=sp){
+                System.out.print("  ");
+                j++;
+            }
+            j=1;
+            while(j<=st){
+                System.out.print("* ");
+                j++;
+            }
+            if (i<=n/2){
+                st--;
+                sp+=2;
+            }
+            else {
+                st++;
+                sp-=2;
+            }
+            System.out.println();
+        }
+
+
+    }
+}
